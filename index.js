48d640ab5925d6107691534dce2c792e6c8fff6d
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-03 23:56:40 +0530

    Final commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const redux = require("redux");
+const createStore = redux.createStore;
+
+/** comment ACTIONS
+ * The only way your application can interact with store
+ * Carry some information form your app to the redux store
+ * Plain Javascript object
+ * Have a 'type' property that describe something that happened in the application
+ * The 'type' property is typically defined as string constants
+ */
+const CAKE_ORDERED = "CAKE_ORDERED";
+
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
+
+/** comment REDUCERS
+ * Specify how the app's state changes in response to actions to actions sent to store
+ * Function that accepts state and action as arguments, and return the next state of the application
+ * (previousState, action) => newState
+ */
+const initialState = {
+  numOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes - 1,
+      };
+
+    default:
+      return state;
+  }
+};
+
+/** comment REDUX STORE
+ * One store for entire application
+ ** Responsibilities --
+ * Holds application state
+ * Allows access to state via getState()
+ * Allows state to be updated via dispatch(action)
+ * Registers listeners via subscribe(listener)
+ * Handles registering of listeners via the function returned by subscribe(listener)
+ */
+const store = createStore(reducer);
+console.log(`Initial State`, store.getState());
+
+store.subscribe(() => console.log(`Update state`, store.getState()));
+
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+// ReferenceError: unsubscribe is not defined
+// unsubscribe()
