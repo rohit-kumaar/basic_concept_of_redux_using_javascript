485900eb2d018b3f728dc719fae0805dd99e440a
Rohit Kumar <94822153+rohitkumarneosoft@users.noreply.github.com>
2022-05-08 19:22:38 +0530

    nested-state : update street using nested state

---
+++ nested-state.js
@@ -0,0 +1,47 @@
+const redux = require("redux");
+
+const initialState = {
+  name: "Ricky",
+  address: {
+    street: "Camp - 2",
+    city: "Bhilai",
+    state: "CG",
+  },
+};
+
+const STREET_UPDATE = "STREET_UPDATE";
+
+const updateStreet = (street) => {
+  return {
+    type: STREET_UPDATE,
+    payload: street,
+  };
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case STREET_UPDATE:
+      return {
+        ...state,
+        address: {
+          ...state.address,
+          street: action.payload,
+        },
+      };
+
+    default: {
+      return state;
+    }
+  }
+};
+
+const store = redux.createStore(reducer);
+console.log(`Initial State`, store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log(`Update State`, store.getState());
+});
+
+store.dispatch(updateStreet("Sector - 20"));
+
+unsubscribe();
